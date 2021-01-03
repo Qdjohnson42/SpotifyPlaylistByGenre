0e0565f87762aaddfc02cbb5375f74d3723ae99d
Q-Dev <>
2021-01-02 21:16:49 -0500

    Continued Progress on Spotify Authorization

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #! /usr/bin/python
 
-from flask import Flask, request, url_for, session, redirect 
+from flask import Flask, request, url_for, session, redirect, jsonify
 import spotipy 
 from spotipy.oauth2 import SpotifyOAuth
 import json
+import time
+import pandas as pd
 
 # Get Secret Tokens Needed
 keys = json.load(open("client_secret.json"))
@@ -34,9 +36,50 @@ def redirectPage():
     session[TOKEN_INFO] = token_info
     return redirect(url_for('getTracks', _external = True))
 
+# Useful Keys That Are Generated From Saved Tracks
+    # items['added_at']
+    # items['external_urls']['uri']
+    # items['external_urls']['name']
+
+
+
 @app.route('/getTracks')
 def getTracks():
-    return "Some Tracks"
+    token_info = ""
+    try:
+        token_info = get_token()
+    except:
+        print("User Not Logged In")
+        return redirect("/")
+
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+
+    all_songs = []
+    iter = 0
+    while True:
+        items = sp.current_user_saved_tracks(limit=50, offset = iter * 50)['items']
+        iter += 1
+        all_songs += items
+
+        if len(items) < 50:
+            break
+
+    return str(len(all_songs))
+
+# Check if Access Token is Expired .. if so Generate Refresh Token and make sure there is token data
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if token_info is None:
+        raise "exception"
+
+    now = int(time.time())
+    is_expired = token_info['expires_at'] - now < 60
+    if (is_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.requests_timeout(token_info['refresh_token'])
+    return token_info
+
 
 def create_spotify_oauth():
     return SpotifyOAuth(
